eb0ba8c6e477baf5283402f016df2733dbe0b3b9
Ruben Barkow-Kuder <github@r.z11.de>
2025-10-22 21:11:56 +0200

    Rename 'Generate Report' to 'Generate List'
    ✏️ Änderung:
    - English: 'Generate Report' → 'Generate List'
    - Deutsch: bleibt 'Ausfallplan generieren' ✅
    
    Cache cleared\! 🇩🇪

--- templates/Schedules/index.php
+++ templates/Schedules/index.php
@@ -31,7 +31,7 @@ $this->assign('title', __('Schedules'));
                     <td><?= __($schedule->state) ?></td>
                     <td><?= h($schedule->created) ?></td>
                     <td class="actions">
-                        <?= $this->Html->link(__('Generate Report'), ['action' => 'generateReport', $schedule->id], ['class' => 'button', 'style' => 'background: #2196F3; color: white;']) ?>
+                        <?= $this->Html->link(__('Generate List'), ['action' => 'generateReport', $schedule->id], ['class' => 'button', 'style' => 'background: #2196F3; color: white;']) ?>
                         <?= $this->Html->link(__('Manage Children'), ['action' => 'manageChildren', $schedule->id], ['class' => 'button']) ?>
                         <?= $this->Html->link(__('Edit'), ['action' => 'edit', $schedule->id]) ?>
                         <?= $this->Form->postLink(__('Delete'), ['action' => 'delete', $schedule->id], ['confirm' => __('Are you sure you want to delete # {0}?', $schedule->id)]) ?>
