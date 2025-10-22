a80cace4c96c0d02f0e5bedffc78a1173577a36e
Ruben Barkow-Kuder <github@r.z11.de>
2025-10-22 19:32:00 +0200

    Switch to BaseMigration for better SQLite compatibility
    Discovery: Old migrations use BaseMigration, new ones use AbstractMigration
    
    Key difference:
    - BaseMigration: Better SQLite support ✅
    - AbstractMigration: Has temp table issues with SQLite ❌
    
    Changed AddDaysCountToSchedules to use BaseMigration.
    Migration already works with raw SQL, but this makes it
    consistent with other migrations in the project.
    
    Updated memory: Use BaseMigration for SQLite migrations!

--- config/Migrations/20251022192317_AddDaysCountToSchedules.php
+++ config/Migrations/20251022192317_AddDaysCountToSchedules.php
@@ -1,9 +1,9 @@
 <?php
 declare(strict_types=1);
 
-use Migrations\AbstractMigration;
+use Migrations\BaseMigration;
 
-class AddDaysCountToSchedules extends AbstractMigration
+class AddDaysCountToSchedules extends BaseMigration
 {
     /**
      * Migrate Up.
