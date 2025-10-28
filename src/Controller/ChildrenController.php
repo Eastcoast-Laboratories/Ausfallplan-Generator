0a9386b57baa5cb71ff30e49ed11827603b0635d
Ruben Barkow-Kuder <github@r.z11.de>
2025-10-28 10:21:16 +0100

    refactor: Remove all legacy entity/table files and references
    Complete removal of deprecated models and associations:
    
    DELETED FILES:
    
    - src/Model/Entity/WaitlistEntry.php
    
    - src/Model/Entity/Assignment.php
    
    - src/Model/Entity/ScheduleDay.php
    
    - src/Model/Table/WaitlistEntriesTable.php
    
    - src/Model/Table/AssignmentsTable.php
    
    - src/Model/Table/ScheduleDaysTable.php
    
    CLEANED ENTITY/TABLE FILES:
    
    1. src/Model/Entity/Child.php
    
       - Removed @property Assignment[]
    
       - Removed @property WaitlistEntry[]
    
       - Removed from _accessible array
    
    2. src/Model/Table/ChildrenTable.php
    
       - Removed @property AssignmentsTable
    
       - Removed @property WaitlistEntriesTable
    
       - Removed hasMany(Assignments)
    
       - Removed hasMany(WaitlistEntries)
    
    3. src/Controller/ChildrenController.php
    
       - Removed WaitlistEntries from contain in view()
    
    RESULT:
    
    Tests: 102, Assertions: 336
    
    Errors: 0 ✅
    
    Failures: 0 ✅
    
    Codebase is now completely clean of legacy table references\!

--- src/Controller/ChildrenController.php
+++ src/Controller/ChildrenController.php
@@ -80,7 +80,6 @@ class ChildrenController extends AppController
         $child = $this->Children->get($id, contain: [
             'Organizations',
             'SiblingGroups',
-            'WaitlistEntries',
         ]);
         
         // Permission check: User must be member of child's organization
